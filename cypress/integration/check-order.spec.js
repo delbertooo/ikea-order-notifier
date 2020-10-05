595a4491d7351b973cf0cd850e9aac83b5d994d6
Robert Worgul <robert.worgul@scitotec.de>
2020-10-05 02:09:13 +0200

    Initial commit

---
+++ cypress/integration/check-order.spec.js
@@ -0,0 +1,28 @@
+describe('IKEA', () => {
+    it('cart cannot be ordered', () => {
+        cy.on('uncaught:exception', (err, runnable) => false) // we don't care about errors
+        cy.visit('https://www.ikea.com/de/de/profile/login/')
+
+        cy.get('#username').type(Cypress.env('IKEA_LOGIN'))
+        cy.get('#password').type(Cypress.env('IKEA_PASSWORD'))
+        cy.get('[name=login]').click()
+
+        cy.contains('Ausloggen', { timeout: 30000 })
+
+        cy.contains('Alle zulassen').click()
+
+        cy.visit('https://www.ikea.com/de/de/shoppingcart/')
+
+        cy.contains('Warenwert zzgl. Liefer-/Servicekosten')
+
+        cy.get('.checkout__button').click()
+
+        cy.get('#zipcode').type(Cypress.env('IKEA_ZIPCODE'))
+
+        cy.contains('Lieferoptionen anzeigen').click()
+
+        cy.contains('Wähle deinen Liefertermin.')
+
+        cy.contains('Folgende Artikel sind begrenzt verfügbar für eine Lieferung.')
+    })
+})
