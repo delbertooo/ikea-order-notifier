91a4eb5455855f798b257fe740c6524d743e528d
Robert Worgul <robert.worgul@scitotec.de>
2020-11-08 20:01:30 +0100

    Re-populate shopping cart every run

--- cypress/integration/check-order.spec.js
+++ cypress/integration/check-order.spec.js
@@ -1,9 +1,25 @@
+function orderList() {
+
+    cy.visit('https://www.ikea.com/de/de/favourites/')
+
+    cy.get('[data-cy-id="open-list-menu"]').click()
+
+    cy.get('[data-cy-id="list-of-lists"]').contains('IKEAOrderNotifier').click()
+
+    cy.wait(1000)
+
+    cy.contains('Online kaufen').click()
+
+    cy.get('.summary [data-cy-id="move-all-to-cart"]').click()
+
+}
+
 describe('IKEA', () => {
     it('cart cannot be ordered', () => {
         cy.on('uncaught:exception', (err, runnable) => false) // we don't care about errors
         cy.visit('https://www.ikea.com/de/de/profile/login/')
         cy.contains('Jetzt registrieren')
-        cy.wait(250) // trying to fix detached errors on headless
+        cy.wait(1000) // trying to fix detached errors on headless
 
         cy.get('#username').type(Cypress.env('IKEA_LOGIN'))
         cy.get('#password').type(Cypress.env('IKEA_PASSWORD'))
@@ -13,7 +29,19 @@ describe('IKEA', () => {
 
         cy.contains('Alle zulassen').click()
 
+        orderList()
+
+
         cy.visit('https://www.ikea.com/de/de/shoppingcart/')
+
+        cy.get(".shoppingbag__title-container .cart-ingka-svg-icon").click()
+
+        cy.contains('Warenkorb leeren').click()
+
+        orderList()
+
+        cy.visit('https://www.ikea.com/de/de/shoppingcart/')
+
 
         cy.contains('Warenwert zzgl. Liefer-/Servicekosten')
 
